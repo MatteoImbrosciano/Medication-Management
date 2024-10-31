845cc9f5c944b73631d9266b3cd641359ba55fe9
wickeet <karimsaidl@correo.ugr.es>
2024-10-31 09:36:30 +0100

    closes MatteoImbrosciano#11, se ha eliminado un método ya que no corresponde en el modelado de datos incluirlo

---
+++ medication_management/medicamento.py
@@ -0,0 +1,7 @@
+class Medicamento:
+    
+    def __init__(self, nombre, cantidad):
+        if cantidad <= 0:
+            raise ValueError("La cantidad debe ser un valor positivo")
+        self.nombre = nombre
+        self.cantidad = cantidad
