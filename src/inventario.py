27c2180daf2d6c81fb95b1a622633de6cd1231ad
wickeet <karimsaidl@correo.ugr.es>
2024-11-07 08:42:16 +0100

    closes MatteoImbrosciano#8, más cambios realizados para respetar la estructura del repositorio y adecuarla al lenguaje de programación

--- src/inventario.py
+++
@@ -1,8 +0,0 @@
-from medicamento import Medicamento
-
-class Inventario:
-    def __init__(self):
-        self.medicamentos = []
-
-    def __str__(self):
-        return "\n".join([str(medicamento) for medicamento in self.medicamentos])
--- src/medicamento.py
+++
@@ -1,9 +0,0 @@
-class Medicamento:
-    def __init__(self, nombre, cantidad):
-        if cantidad <= 0:
-            raise ValueError("La cantidad debe ser un valor positivo")
-        self.nombre = nombre
-        self.cantidad = cantidad
-
-    def __str__(self):
-        return f"{self.nombre} - {self.cantidad}"
