acfd326e92810a2c8e12dc899b00088c84f36e07
Matteo <1000014829@studium.unict.it>
2024-11-27 10:54:31 +0100

    aggiornamento del file in un unica riga. closed #16

--- check_syntax.py
+++ check_syntax.py
@@ -1,17 +1,1 @@
-import os
-import py_compile
-
-def check_syntax():
-    for root, _, files in os.walk("."):
-        for file in files:
-            if file.endswith(".py"):
-                file_path = os.path.join(root, file)
-                try:
-                    py_compile.compile(file_path, doraise=True)
-                    print(f"✔ Syntax OK: {file_path}")
-                except py_compile.PyCompileError as e:
-                    print(f"✘ Syntax Error: {file_path}")
-                    print(e)
-
-if __name__ == "__main__":
-    check_syntax()
+import os, py_compile; [print(f"✔ Sintassi OK: {os.path.join(root, file)}") if py_compile.compile(os.path.join(root, file), doraise=True) else None for root, _, files in os.walk(".") for file in files if file.endswith(".py")]
