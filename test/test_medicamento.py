86377c5fb8e20b64a9cb2fa078de82cd4895b781
MatteoImbrosciano <82762466+MatteoImbrosciano@users.noreply.github.com>
2024-12-05 10:07:08 +0100

    Update test_medicamento.py, #22

--- test/test_medicamento.py
+++ test/test_medicamento.py
@@ -2,11 +2,17 @@ from medication_management.medicamento import Medicamento
 import pytest
 
 def test_medicamento_valido():
-    medicamento = Medicamento("Paracetamol", 10, 5.99)
+    medicamento = Medicamento("Paracetamol", 10, 5.99, "500 mg")
     assert medicamento.nombre == "Paracetamol"
     assert medicamento.cantidad == 10
     assert medicamento.precio == 5.99
+    assert medicamento.unidad == "500 mg"
 
 def test_medicamento_invalido():
+    # Test quantità non valida
     with pytest.raises(ValueError):
-        Medicamento("Ibuprofeno", 0, 4.99)  # Quantità non valida
+        Medicamento("Ibuprofeno", 0, 4.99, "200 mg")
+    
+    # Test prezzo non valido
+    with pytest.raises(ValueError):
+        Medicamento("Ibuprofeno", 10, -4.99, "200 mg")
