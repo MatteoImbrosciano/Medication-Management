874eba0b9a542c686de1db808b7748337fac3daa
Matteo <1000014829@studium.unict.it>
2024-12-12 12:02:09 +0100

    #22, Aggiunti test unitari per la classe Medicamento. Verificata la gestione del parametro unitad aggiunto nella classe Medicamento. Scritti test per casi validi e per eccezioni: Prezzo negativo, unitad negativo,unitad uguale a zero.

--- test/test_medicamento.py
+++ test/test_medicamento.py
@@ -1,25 +1,21 @@
-import unittest
+import pytest
 from medication_management.medicamento import Medicamento
 
-class TestMedicamento(unittest.TestCase):
+def test_medicamento_valido():
+    medicamento = Medicamento(nombre="Paracetamolo", cantidad="50 mg", precio=0.50, unitad=2)
+    assert medicamento.nombre == "Paracetamolo"
+    assert medicamento.cantidad == "50 mg"
+    assert medicamento.precio == 0.50
+    assert medicamento.unitad == 2
 
-    def test_creazione_medicamento_valido(self):
-        m = Medicamento("Aspirina", 10, 2.5, "pz")
-        self.assertEqual(m.nombre, "Aspirina")
-        self.assertEqual(m.cantidad, 10)
-        self.assertEqual(m.precio, 2.5)
-        self.assertEqual(m.unidad, "pz")
+def test_medicamento_cantidad_negativa():
+    with pytest.raises(ValueError, match="El precio debe ser un valor numérico positivo"):
+        Medicamento(nombre="Ibuprofeno", cantidad="10 mg", precio=-0.30, unitad=1)
 
-    def test_cantidad_invalida(self):
-        with self.assertRaises(ValueError) as context:
-            Medicamento("Aspirina", -1, 2.5, "pz")
-        self.assertIn("La cantidad debe ser un valor numérico positivo", str(context.exception))
+def test_medicamento_unitad_negativa():
+    with pytest.raises(ValueError, match="El unitad debe ser un valor positivo"):
+        Medicamento(nombre="Aspirina", cantidad="20 mg", precio=0.20, unitad=-1)
 
-    def test_precio_invalido(self):
-        with self.assertRaises(ValueError) as context:
-            Medicamento("Aspirina", 10, -2.5, "pz")
-        self.assertIn("El precio debe ser un valor numérico positivo", str(context.exception))
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_medicamento_unitad_zero():
+    with pytest.raises(ValueError, match="El unitad debe ser un valor positivo"):
+        Medicamento(nombre="Aspirina", cantidad="20 mg", precio=0.20, unitad=0)
