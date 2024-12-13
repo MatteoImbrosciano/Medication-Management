8fe4740f2fe121c0f6a8805b409e1df691158749
Matteo <1000014829@studium.unict.it>
2024-12-13 17:24:45 +0100

    #22, scritti test per far vedere agli utenti i medicinali acquistati

--- test/test_medicamento.py
+++ test/test_medicamento.py
@@ -8,14 +8,10 @@ def test_medicamento_valido():
     assert medicamento.precio == 0.50
     assert medicamento.unitad == 2
 
-def test_medicamento_cantidad_negativa():
+def test_medicamento_precio_negativo():
     with pytest.raises(ValueError, match="El precio debe ser un valor numérico positivo"):
         Medicamento(nombre="Ibuprofeno", cantidad="10 mg", precio=-0.30, unitad=1)
 
 def test_medicamento_unitad_negativa():
     with pytest.raises(ValueError, match="El unitad debe ser un valor positivo"):
         Medicamento(nombre="Aspirina", cantidad="20 mg", precio=0.20, unitad=-1)
-
-def test_medicamento_unitad_zero():
-    with pytest.raises(ValueError, match="El unitad debe ser un valor positivo"):
-        Medicamento(nombre="Aspirina", cantidad="20 mg", precio=0.20, unitad=0)
