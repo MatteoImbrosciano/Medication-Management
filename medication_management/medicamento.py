08ea24a6c879891e096c627672ad754a24eea929
Matteo <1000014829@studium.unict.it>
2024-12-13 17:20:06 +0100

    aggiunta di unitad

--- medication_management/medicamento.py
+++ medication_management/medicamento.py
@@ -1,11 +1,16 @@
 class Medicamento:
-    
-    def __init__(self, nombre, cantidad, precio, unidad):
-        if cantidad <= 0:
-            raise ValueError("La cantidad debe ser un valor numérico positivo")
+    def __init__(self, nombre, cantidad, precio, unitad):
+
         if precio <= 0:
             raise ValueError("El precio debe ser un valor numérico positivo")
+        if not isinstance(unitad, (int, float)) or unitad <= 0:
+            raise ValueError("El unitad debe ser un valor positivo")
+        
+        # Validazione opzionale per cantidad
+        if not isinstance(cantidad, str) or not cantidad.strip():
+            raise ValueError("La cantidad debe ser una cadena no vacía")
+        
         self.nombre = nombre
-        self.cantidad = cantidad
+        self.cantidad = cantidad  
         self.precio = precio
-        self.unidad = unidad
+        self.unitad = unitad
