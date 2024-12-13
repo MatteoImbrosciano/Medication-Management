f2bc967b501c2c3925f79ca384706c44fa3950a2
Matteo <1000014829@studium.unict.it>
2024-12-13 17:19:32 +0100

    #22, definite dei metodi per visualizzare l'inventario

--- medication_management/inventario.py
+++ medication_management/inventario.py
@@ -1,5 +1,7 @@
 from medication_management.medicamento import Medicamento
 
+from pathlib import Path
+
 class Inventario:
     def __init__(self):
         self.medicamentos = []
@@ -16,3 +18,21 @@ class Inventario:
             [f"{m.nombre}: Cantidad={m.cantidad}, Precio={m.precio}€, Unitad={m.unitad}" for m in self.medicamentos]
         )
         return inventario
+
+    def importar_desde_txt(self, ruta: str):
+        file_path = Path(ruta)
+        if not file_path.exists():
+            raise FileNotFoundError(f"El archivo {ruta} no existe.")
+        contenido = file_path.read_text(encoding='utf-8')
+        lineas = contenido.strip().split("\n")
+        for linea in lineas:
+            partes = linea.split("|")
+            if len(partes) == 4:  
+                nombre, cantidad, unitad, precio = partes
+                medicamento = Medicamento(
+                    nombre=nombre.strip(),
+                    cantidad=cantidad.strip(),
+                    unitad=int(unitad.strip()),
+                    precio=float(precio.strip())
+                )
+                self.agregar_medicamento(medicamento)
