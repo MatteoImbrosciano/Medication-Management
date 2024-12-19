8787a46c16810f84c8cba42eeb1c9b6d55d3d442
Matteo <1000014829@studium.unict.it>
2024-12-19 19:27:44 +0100

    fatta diventare una dataclss

--- medication_management/medicamento.py
+++ medication_management/medicamento.py
@@ -1,18 +1,19 @@
+from dataclasses import dataclass
+
+@dataclass
 class Medicamento:
-    def __init__(self, nombre, cantidad, precio, unitad):
-        if not isinstance(nombre, str) or not nombre.strip():
+    nombre: str
+    cantidad: float
+    precio: float
+    unitad: str  
+    
+    def __post_init__(self):
+        if not isinstance(self.nombre, str) or not self.nombre.strip():
             raise ValueError("El nombre debe ser una cadena no vacía.")
-        if cantidad <= 0:
+        if self.cantidad <= 0:
             raise ValueError("La cantidad debe ser un valor numérico positivo.")
-        if precio <= 0:
+        if self.precio <= 0:
             raise ValueError("El precio debe ser un valor numérico positivo.")
-        if not isinstance(unitad, str) or not unitad.strip():
+        if not isinstance(self.unitad, str) or not self.unitad.strip():
             raise ValueError("La unidad debe ser una cadena no vacía.")
-
-        self.nombre = nombre.strip()
-        self.cantidad = cantidad
-        self.precio = precio
-        self.unitad = unitad.strip()
-
-    def __str__(self):
-        return f"{self.nombre} ({self.cantidad} {self.unitad}): €{self.precio:.2f}"
+        
