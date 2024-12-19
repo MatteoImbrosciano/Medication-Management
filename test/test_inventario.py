366d4fe6f18e1f1d65e09c2455773d96b3b6634f
MatteoImbrosciano <82762466+MatteoImbrosciano@users.noreply.github.com>
2024-12-19 17:46:41 +0100

    Delete test directory

--- test/test_inventario.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from medication_management.inventario import Inventario
-from medication_management.medicamento import Medicamento
-
-def test_agregar_y_obtener_inventario():
-    inventario = Inventario()
-    med = Medicamento("Ibuprofeno", 10, 3.0, "mg")
-    inventario.agregar_medicamento(med)
-    assert inventario.obtener_inventario() == [("Ibuprofeno", 10, "mg")]
